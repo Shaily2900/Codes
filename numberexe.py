f76c37edeff50a4e6a46941249998d9bbde55936
Shaily2900 <67729452+Shaily2900@users.noreply.github.com>
2021-09-12 11:39:33 +0530

    Delete numberexe.py

--- numberexe.py
+++
@@ -1,6 +0,0 @@
-import math
-a=float(input("Enter the radius of the circle:"))
-area= math.pi*(a**2)
-circumference= 2*math.pi*a
-print("Area",round(area,2))
-print("Circumferencee:",round(circumference,2))
