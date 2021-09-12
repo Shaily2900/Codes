83314c640d346550e4a7c213c10bd0c8ddbcf412
Shaily2900 <67729452+Shaily2900@users.noreply.github.com>
2021-09-12 11:37:45 +0530

    Delete km to miles.py

--- km to miles.py
+++
@@ -1,5 +0,0 @@
-print("To convert the kilometers into miles")
-distance=float(input("Enter the distance covered in kilometers:"))
-miles=distance/1.609344
-print("The distance covered in miles is:",miles)
-
