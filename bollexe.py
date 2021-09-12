9f50c3b196956379d9cc2bb4e5970038b7fddf19
Shaily2900 <67729452+Shaily2900@users.noreply.github.com>
2021-09-12 11:37:10 +0530

    Delete bollexe.py

--- bollexe.py
+++
@@ -1,8 +0,0 @@
-my_age=19
-his_age=int(input("Enter his age:"))
-if(my_age > his_age):
- print("He's younger than you")
-elif(my_age < his_age):
- print("He's older than you")
-else:
- print("You two have the same age")
