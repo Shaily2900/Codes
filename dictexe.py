8a2d37b758951334eb6f77cffffa9cd697a3a52e
Shaily2900 <67729452+Shaily2900@users.noreply.github.com>
2021-09-12 11:37:21 +0530

    Delete dictexe.py

--- dictexe.py
+++
@@ -1,4 +0,0 @@
-Dict={'name':'abc','age':'40','profession':'doctor','address':'guj','phone':'326'}
-x=input("Enter the details you want about person:").lower()
-result=Dict.get(x,"Detail not present")
-print(result)
